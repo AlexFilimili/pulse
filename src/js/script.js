d3e1960f4a814ed02f7f9533953a998f812bcaa3
AlexFili <RootyRoots1507@gmail.com>
2022-12-11 19:27:35 +0300

    block 4 is ready

---
+++ src/js/script.js
@@ -0,0 +1,68 @@
+/* let answer = confirm("Вам есть 18?");
+console.log(answer); */
+
+/* console.log(4+'fff'); */
+
+/* let isChecked = false,
+    isClose = false; */
+
+/* console.log(isChecked && isClose); */ /* оператор и */
+/* console.log(isChecked || isClose);  *//* оператор или */
+
+/* if (3*5 == 9+8) {
+    console.log('Верно');
+} else {
+    console.log('Ошибка')
+} */
+
+/* let answer = confirm("Вам есть 18?");
+    if (answer) {
+        console.log('заходь');
+    }
+    else {
+        console.log('тебе нельзя');
+    } */
+
+/*    const num = 5;
+    if (num<4) {
+        console.log('мало');
+    }
+    else if (num>7) {
+        console.log('много');
+    }
+    else if (num>9) {
+        console.log('много');
+    }
+    else {
+        console.log('верно');
+    } */
+
+/*     for (let i = 1; i < 16; i++) {
+        console.log(i);
+    }
+ */
+
+/*     function logging (a,b) {
+        console.log(a*b);
+    }
+    logging(2,5);
+    logging(3,6); */
+
+$(document).ready(function(){
+    $('.carusel__inner').slick({
+        speed: 1300,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevron_left_solid.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron_right_solid.png"></button>',
+        responsive: [
+            {
+              breakpoint: 900,
+              settings: {
+                arrows: false,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+              }
+              }
+        ]
+    });
+    });
